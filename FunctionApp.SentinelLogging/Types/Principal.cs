1872321957b4644f4be25fd0e404685bf8a597c7
Laura Kokkarinen <Laura.Kokkarinen@sulava.com>
2025-04-16 12:29:20 +0300

    Added events to log.

---
+++ FunctionApp.SentinelLogging/Types/Principal.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FunctionApp.SentinelLogging.Types
+{
+    public class Principal
+    {
+        [JsonPropertyName("auth_typ")]
+        public string? AuthType { get; set; }
+        [JsonPropertyName("claims")]
+        public List<Claim>? Claims { get; set; }
+        [JsonPropertyName("name_typ")]
+        public string? NameType { get; set; }
+        [JsonPropertyName("role_typ")]
+        public string? Roletype { get; set; }
+    }
+
+    public class Claim
+    {
+        [JsonPropertyName("typ")]
+        public string? Type { get; set; }
+        [JsonPropertyName("val")]
+        public string? Value { get; set; }
+    }    
+}
